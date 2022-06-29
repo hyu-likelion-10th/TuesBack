1db3bfbf1fe94a30aea8867c18874e4d32c1ba99
hanuel <ezzeezz@naver.com>
2022-06-29 20:04:17 +0900

    [add]gitignore

--- week7/blogproject/settings.py
+++ week7/blogproject/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os, json
+from django.core.exceptions import ImproperlyConfigured
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,8 +21,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-axvf)_q=5vkw1d24bll7gg=u0f-37)q3uqw+*-u5jzc^%yb#y1'
+secret_file = os.path.join(BASE_DIR, 'secrets.json') 
 
+with open(secret_file, 'r') as f: 
+    secrets = json.loads(f.read())
+
+def get_secret(setting, secrets=secrets): 
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
